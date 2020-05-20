ccf647dc9c1d5db5e5a0cc6a26b3572a3a647d22
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-05-19 22:54:57 -0500

    modifiying user entity

---
+++ ASPNETCoreIdentity/AspNetCoreIdentity/AspNetCoreIdentity/Data/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace AspNetCoreIdentity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData] //This tag allows users to get all information tagged with the tag; Also when a users deletes a account all the data tagged as PersonalData gets Deleted
+        public DateTime CareerStarted { get; set; }
+
+    }
+}
--- ASPNETCoreIdentity/AspNetCoreIdentity/AspNetCoreIdentity/Data/Migrations/20200520035357_User-CareerStarted.Designer.cs
+++ ASPNETCoreIdentity/AspNetCoreIdentity/AspNetCoreIdentity/Data/Migrations/20200520035357_User-CareerStarted.Designer.cs
@@ -4,14 +4,16 @@ using AspNetCoreIdentity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AspNetCoreIdentity.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200520035357_User-CareerStarted")]
+    partial class UserCareerStarted
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
