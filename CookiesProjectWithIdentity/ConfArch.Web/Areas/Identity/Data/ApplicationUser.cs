4883f70f1da56117cd7e33f33513773deed6e5af
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-05-23 11:53:51 -0500

    adding project with Identity

---
+++ CookiesProjectWithIdentity/ConfArch.Web/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace AspNetCoreIdentity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData] //This tag allows users to get all information tagged with the tag; Also when a users deletes a account all the data tagged as PersonalData gets Deleted
+        public DateTime CareerStarted { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
