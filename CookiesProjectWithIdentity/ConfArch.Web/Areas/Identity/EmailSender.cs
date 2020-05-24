f17dd277d9652a9a2b27beafff3622f8f0db5b91
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-05-24 15:04:10 -0500

    adding EmailSender

---
+++ CookiesProjectWithIdentity/ConfArch.Web/Areas/Identity/EmailSender.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Threading.Tasks;
+
+namespace ConfArch.Web.Areas.Identity
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // You can implement this via SendGrid.com
+            throw new System.NotImplementedException();
+        }
+    }
+}
